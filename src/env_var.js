1d1d091c6bb876f481bb6730109461e4c649b7f7
chaitanya-bhargava <chaitanyabhargava5@gmail.com>
2023-05-29 21:59:13 +0530

    config fix

---
+++ src/env_var.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5HTyXdtoLSCPIjwMMsqnxybVG1sFLIB8",
+    authDomain: "mediaplayer-94ca2.firebaseapp.com",
+    projectId: "mediaplayer-94ca2",
+    storageBucket: "mediaplayer-94ca2.appspot.com",
+    messagingSenderId: "471225334990",
+    appId: "1:471225334990:web:18361423160d35ed7d649f"
+  };
+
+export default firebaseConfig;
